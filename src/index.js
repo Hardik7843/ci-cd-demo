e7fbfa9d194422f5118ec20d165934e12bb37f7c
Hardik7843 <hardikprajapati7843@gmail.com>
2026-06-14 22:16:25 +0530

    Update response message to indicate version 2 for re-triggering the run on ubuntu

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message : 'Re-Triggering the Run on ubuntu with change'})
+  res.status(200).json({ message : 'Re-Triggering the Run on ubuntu with change v2'})
 })
 
 
