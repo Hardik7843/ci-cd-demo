50a7267cc9dd4e1d3e7d8152f37715ae822523de
Hardik-BlkCrnt <hardikp.blackcurrant@gmail.com>
2026-06-15 11:24:00 +0530

    waking up auto matic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message : 'Re-Triggering the Run on ubuntu with change v2'})
+  res.status(200).json({ message : 'Instance woke up'})
 })
 
 
